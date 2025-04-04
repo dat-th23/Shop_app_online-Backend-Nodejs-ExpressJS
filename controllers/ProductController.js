005398b57ae2c687626bb53dfb282bfe56f7a868
dat-th23 <dat.th1230@gmail.com>
2025-04-04 23:40:22 +0700

    get product by id and get all products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -3,14 +3,27 @@ import db from "../models"
 import InsertProductRequest from "../dtos/requests/InsertProductRequest"
 
 export async function getAllProducts(req, res) {
+    const products = await db.Product.findAll()
     res.status(200).json({
-        message: 'Get product list successfully'
+        message: 'Get product list successfully',
+        data: products
     })
 }
 
 export async function getProductById(req, res) {
+    const { id } = req.params
+    const product = await db.Product.findByPk(id)
+
+    if (!product) {
+        // If no product found, return a 404 Not Found Response
+        return res.status(404).json({
+            message: 'Not Found!',
+            data: product
+        })
+    }
     res.status(200).json({
-        message: 'Get product by id successfully'
+        message: 'Get product by id successfully',
+        data: product,
     })
 }
 
